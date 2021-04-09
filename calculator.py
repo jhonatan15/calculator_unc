8c471dddc29d3e4df77555e7a2780533125760f6
jhonatan <>
2021-04-08 21:04:11 -0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,36 @@
+def sum(a, b):
+    print(a + b)
+    return a + b
+
+def substraction(a, b):
+    print(a - b)
+    return a - b
+
+def multiply(a, b):
+    print(a * b)
+    return a * b
+
+def divide(a, b):
+    print(a / b)
+    return a / b
+
+def power(a, b):
+    print(a ** b)
+    return a ** b
+
+def greet(name):
+    print ("Hello " + name)
+    return "Hello " + name
+
+def goodbye(name):
+    print("Goodbye " + name)
+    return "Goodbye " + name
+
+
+greet("Jhonatan")
+multiply(2, 3)
+sum(2, 3)
+substraction(2, 3)
+divide(2, 3)
+power(2, 3)
+goodbye("Jhonatan")
